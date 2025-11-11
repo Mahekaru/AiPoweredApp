bec4b916990bf49673f6e27c9062d41ec3d6b33f
Mahekaru Hitokui <mahekaru@gmail.com>
2025-11-11 13:54:16 -0500

    adding prettier

--- App/packages/client/src/App.tsx
+++ App/packages/client/src/App.tsx
@@ -2,20 +2,20 @@ import { useEffect, useState } from "react";
 import { Button } from "./components/ui/button";
 
 function App() {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   useEffect(() => {
-    fetch('http://localhost:3000/api/hello')
-      .then(response => response.json())
-      .then(data => setMessage(data.message))
-      .catch(error => console.error('Error fetching message:', error));
-  },[]);
+    fetch("http://localhost:3000/api/hello")
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error("Error fetching message:", error));
+  }, []);
 
   return (
     <>
-      <p className="font-bold p-4 text-3xl" >{message}</p>
+      <p className="font-bold p-4 text-3xl">{message}</p>
       <Button className="m-4">Click ME</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
